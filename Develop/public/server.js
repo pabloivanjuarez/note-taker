b7d284d8c446a28ffa875161a8e7fe6f65037255
pablito <pabloivanjuarez@gmail.com>
2020-09-22 23:07:17 -0700

    Express and Port added to server.js

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -2,3 +2,7 @@
 const express = require("express")
 const path = require("path")
 const fs = require("fs")
+
+// using express and opening a port to run server
+const app = express()
+const PORT = 3000
