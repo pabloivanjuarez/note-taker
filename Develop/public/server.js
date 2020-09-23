72e06c39cede6291a1f8c8e200e5d6e1bcf1275d
pablito <pabloivanjuarez@gmail.com>
2020-09-22 23:02:53 -0700

    server.js created

---
+++ Develop/public/server.js
@@ -0,0 +1,8 @@
+// Depnendencies
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+
+// using express and opening a port to run server
+const app = express()
+const PORT = 3000
