2797fe4c8d1237bdfe7fb11d2d499c49f62bf6c5
pablito <pabloivanjuarez@gmail.com>
2020-09-23 22:50:03 -0700

    express static

--- Develop/server.js
+++ Develop/server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+app.use(express.static("public"));
 // require("./routes/htmlRoutes")(app)
 app.use("/", htmlRoutes)
 
