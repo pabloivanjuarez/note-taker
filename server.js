01401a485c8b16945c572f6afdfc145064a7ed6f
pablito <pabloivanjuarez@gmail.com>
2020-09-25 17:07:05 -0700

    changes to server.js and notes.html

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 // require("./routes/htmlRoutes")(app)
 app.use("/", htmlRoutes)
 
