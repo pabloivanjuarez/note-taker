af4bc8dccb1ccad8ff26caa464af19ca78c7780b
pablito <pabloivanjuarez@gmail.com>
2020-09-26 10:28:49 -0700

    uuid added

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const router = require("express").Router()
 
 router.get("/getNotes", function (req, res) {
-  let data = fs.readFileSync("../db/db", "utf-8");
+  let data = fs.readFileSync("develop/db/db", "utf-8");
   data = JSON.parse(data)
   res.send(data)
 })
